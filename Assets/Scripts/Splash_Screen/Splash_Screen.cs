3468ddfbfd1bda5ab0f6d3ce252da017306c9534
Stephen Corwin <corwins@starssite.com>
2013-06-11 17:04:26 -0400

    rawr

--- Assets/Scripts/Splash_Screen/Splash_Screen.cs
+++ Assets/Scripts/Splash_Screen/Splash_Screen.cs
@@ -9,22 +9,29 @@ public class Splash_Screen : MonoBehaviour
 	public GUIStyle SplashStyle = new GUIStyle();
 	private int _BTN_X;
 	private int _BTN_Y;
-	
+	public GameObject MovieScreen;
+	public MovieTexture SplashMovie;
 	// Use this for initialization
 	void Start ()
 	{
 		_BTN_X = Screen.width/2-BTN_Width/2;
 		_BTN_Y = Screen.height/2-BTN_Height/2;
+		
+		MovieScreen.GetComponent<MeshRenderer>().materials[0].mainTexture = SplashMovie;
+		//MovieScreen.GetComponent<MeshRenderer>().material.mainTexture.Loop= true;;
+		SplashMovie.loop = true;
+		SplashMovie.Play();
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		
 	}
 	
 	void OnGUI()
-	{
+	{			
 		//Creates a button based off of the dimensions of the HUD class
 		if (GUI.Button (new Rect (0,0,Screen.width,Screen.height),BTN_Text,SplashStyle))
 			{
